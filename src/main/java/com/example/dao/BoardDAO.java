ca5d4ddd8f59256673cb0c5bb5dbb4a81a59f1a0
조상근 <22000713@handong.ac.kr>
2023-12-01 22:14:45 +0900

    first commit

--- src/main/java/com/example/dao/BoardDAO.java
+++ src/main/java/com/example/dao/BoardDAO.java
@@ -1,2 +1,60 @@
-package com.example.dao;public class BoardDAO {
+package com.example.dao;
+
+import com.example.bean.BoardVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@Repository
+public class BoardDAO {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+    Connection conn = null;
+    PreparedStatement stmt = null;
+    ResultSet rs = null;
+
+    public int insertBoard(BoardVO vo){
+        String sql = "insert into BOARD (title, writer, content) values ("+"'"+vo.getTitle()+"',"+"'"+vo.getWriter()+"',"+"'"+vo.getContent()+"')";
+        return jdbcTemplate.update(sql);
+    }
+
+    public int deleteBoard(int seq){
+        String sql="delete from BOARD where seq = " + seq;
+        return jdbcTemplate.update(sql);
+    }
+
+    public int updateBoard(BoardVO vo){
+        String sql = "update BOARD set title='" + vo.getTitle() + "'," + "title='"+vo.getWriter()+"',"+"writer="+vo.getWriter()+"',"+"content='"+vo.getContent()+"'where seq="+vo.getSeq();
+        return jdbcTemplate.update(sql);
+    }
+
+    class BoardRowMapper implements RowMapper<BoardVO> {
+        @Override
+        public BoardVO mapRow(ResultSet rs, int rowNum) throws SQLException {
+         BoardVO vo = new BoardVO();
+         vo.setSeq(rs.getInt("seq"));
+         vo.setTitle(rs.getString("title"));
+         vo.setContent(rs.getString("content"));
+         vo.setRegdate(rs.getDate("regdate"));
+         return vo;
+        }
+    }
+
+    public BoardVO getBoard(int seq) {
+        String sql = "select * from SpringBOARD where seq=" + seq;
+        return jdbcTemplate.queryForObject(sql, new BoardRowMapper());
+    }
+
+    public List<BoardVO> getBoardList() {
+        String sql = "select * from SpringBOARD order by regdate desc";
+        return jdbcTemplate.query(sql, new BoardRowMapper());
+    }
 }
